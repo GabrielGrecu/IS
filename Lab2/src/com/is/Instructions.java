0b616355afa7b24ae718d7670f2e5c4a96f6fd94
Gabriel Grecu <grecualexandrugabriel@yahoo.com>
2024-02-26 12:40:08 +0200

    second commit

---
+++ Lab2/src/com/is/Instructions.java
@@ -0,0 +1,13 @@
+package com.is;
+
+public class Instructions {
+    void afisare(){
+        System.out.println("Hello world");
+    }
+
+    void info(){
+        System.out.println("Facultatea de automatica si calculatoare");
+        System.out.println("Laboratoare Java");
+        System.out.println("Grecu Gabriel");
+    }
+}
--- Lab2/src/com/is/Main.java
+++ Lab2/src/com/is/Main.java
@@ -1,7 +1,12 @@
 package com.is;
 
 public class Main {
+
     public static void main(String[] args) {
+        Instructions instructions = new Instructions();
+        instructions.afisare();
+        instructions.info();
+
         System.out.println("lll");
     }
 }
