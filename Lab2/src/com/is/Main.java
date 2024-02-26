53a69e65c60f5dead8272d866e37a0ea78c7e61c
Gabriel Grecu <grecualexandrugabriel@yahoo.com>
2024-02-26 12:45:46 +0200

    third commit

--- Lab2/src/com/is/Main.java
+++ Lab2/src/com/is/Main.java
@@ -1,9 +1,31 @@
 package com.is;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Instructions instructions = new Instructions();
+
+        while (true) {
+            System.out.println("Introduceți un string ('hello', 'info', 'exit'): ");
+
+            String input = scanner.nextLine();
+
+            if (input.equals("hello")) {
+                instructions.afisare();
+            } else if (input.equals("info")) {
+                instructions.info();
+            } else if (input.equals("exit")) {
+                break;
+            } else {
+                System.out.println("Stringul citit: " + input);
+            }
+        }
+
+        scanner.close();
+        
         instructions.afisare();
         instructions.info();
 
